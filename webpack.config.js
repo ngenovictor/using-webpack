fa859892efc699b05f5ea8973fb5209a46b7e60b
Victor Ngeno <victor.ngeno@gro-intelligence.com>
2019-02-18 09:42:29 +0300

    add linting capabilitites

--- webpack.config.js
+++ webpack.config.js
@@ -27,6 +27,11 @@ module.exports = {
             {
                 test: /\.css$/,
                 use: ['style-loader', 'css-loader']
+            }, 
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loader: "eslint-loader"
             }
         ]
     }
